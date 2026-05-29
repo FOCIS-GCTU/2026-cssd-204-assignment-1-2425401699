051a98d895c5c72337b3c90402b08ea3b8da506f
2425401699 <2425401699@live.gctu.edu.gh>
2026-05-28 21:14:31 -1200

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,6 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Darkwa Gyimah Ishmael
+# STUDENT ID: 2425401699
+# Email: ishmaelgyimah13@gmail.com
+# Grader: Mr.Augustus Buckman
+#
+# On my honor, Darkwa Gyimah Ishmael, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters
+
+    print()
+    print("...DGI")
+    print()
+
+    print("...DDDDDDDDDD........GGGGGGGGGG........IIIIIIIIIIII....")
+    print("...DD.......DD.......GG.......GG...........II..........")
+    print("...DD........DD......GG....................II..........")
+    print("...DD........DD......GG....................II..........")
+    print("...DD........DD......GG..GGGGGG............II..........")
+    print("...DD........DD......GG......GG............II..........")
+    print("...DD........DD......GG......GG............II..........")
+    print("...DD.......DD..**...GGGGGGGGGG...**.......II........**")
+    print("...DDDDDDDDDD...**...GGGGGGGGGG...**...IIIIIIIIIIII..**")
+    print(".......................................................")
+
+    print()
 
 
 
